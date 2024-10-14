6779bb472c80418fba9ba1f12464961d3edd91ea
hnscrft <hnnsmncrft@gmail.com>
2024-10-14 14:19:12 +0200

    Added boosting

--- Assets/Scripts/PlayerBaviour.cs
+++ Assets/Scripts/PlayerBaviour.cs
@@ -4,20 +4,31 @@ using UnityEngine;
 
 public class PlayerBaviour : MonoBehaviour
 {
-    [SerializeField] float rotationSpeed = -5;
+    [SerializeField] float moveForce = 5f;
+    [SerializeField] float boostTime = 50f;
+    [SerializeField] float boostMultiplier = 2;
+
+    bool isBoosting = false;
+
+    private Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        isBoosting = Input.GetKey(KeyCode.Space);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D))
+        if(isBoosting == false)
         {
-            transform.Rotate(0, 0, rotationSpeed * Input.GetAxisRaw("Horizontal") * Time.deltaTime);
+            rb.AddForce(new Vector2(moveForce * Time.deltaTime * Input.GetAxisRaw("Horizontal"), moveForce * Time.deltaTime * Input.GetAxisRaw("Vertical"))); //Moves player
         }
-        
+        else if (isBoosting == true && boostTime != 0)
+        {
+            rb.AddForce(new Vector2( moveForce * boostMultiplier * Time.deltaTime * Input.GetAxisRaw("Horizontal"), moveForce * boostMultiplier * Time.deltaTime * Input.GetAxisRaw("Vertical")));
+            boostTime -= 1 * Time.deltaTime;
+        }
     }
 }

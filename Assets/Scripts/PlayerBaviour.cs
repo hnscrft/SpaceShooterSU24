0d9226293f77fb62f2e519f0fe5de7ccda07a65b
hnscrft <hnnsmncrft@gmail.com>
2024-10-14 11:06:54 +0200

    Deleted Button scrypt

--- Assets/Scripts/PlayerBaviour.cs
+++ Assets/Scripts/PlayerBaviour.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Button : MonoBehaviour
+public class PlayerBaviour : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -13,9 +13,6 @@ public class Button : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.anyKeyDown)
-        {
-            Debug.Log("Someone pressed a button");
-        }
+        
     }
 }
